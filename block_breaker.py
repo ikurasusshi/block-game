3705806f4b4992a814cf35aa657509a615ac5f67
ikurasusshi <kaikai180208@gmail.com>
2025-08-17 19:42:17 +0900

    Create simple-game

--- block_breaker.py
+++ block_breaker.py
@@ -35,3 +35,59 @@ for i in range(5):  # 5行
 
 
 clock = pygame.time.Clock()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+    # キー入力
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and paddle.left > 0:
+        paddle.move_ip(-5, 0)
+    if keys[pygame.K_RIGHT] and paddle.right < WIDTH:
+        paddle.move_ip(5, 0)
+
+
+    # ボール移動
+    ball.move_ip(ball_dx, ball_dy)
+
+
+    # 壁で反射
+    if ball.left <= 0 or ball.right >= WIDTH:
+        ball_dx = -ball_dx
+    if ball.top <= 0:
+        ball_dy = -ball_dy
+
+
+    # パドルで反射
+    if ball.colliderect(paddle):
+        ball_dy = -ball_dy
+
+
+    # ブロックに当たったら消す
+    hit_index = ball.collidelist(blocks)
+    if hit_index != -1:
+        hit_block = blocks.pop(hit_index)
+        ball_dy = -ball_dy
+
+
+    # 下に落ちたらゲームオーバー
+    if ball.bottom >= HEIGHT:
+        print("ゲームオーバー！")
+        pygame.quit()
+        sys.exit()
+
+
+    # 描画
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, BLUE, paddle)
+    pygame.draw.ellipse(screen, WHITE, ball)
+    for block in blocks:
+        pygame.draw.rect(screen, RED, block)
+
+    pygame.display.flip()
+    clock.tick(60)
